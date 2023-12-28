4e17087045956de0939987c1a7067aa7bd3b0615
Klebson Amarante <klebson.silva@aluno.uepb.edu.br>
2023-12-28 01:47:15 -0300

    atualizando

--- menu.js
+++ menu.js
@@ -14,3 +14,27 @@ menu.addEventListener('click', ()=>{
 overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
 })
+
+function fecharFormulario() {
+    var formulario = document.getElementById("form-contato");
+    formulario.classList.remove("show-form");
+
+    var overlayMenu = document.getElementById("overlay-menu");
+    overlayMenu.classList.remove("show-overlay");
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const btnForm = document.getElementById('btn-form');
+    const formContato = document.getElementById('form-contato');
+    const overlayMenu = document.getElementById('overlay-menu');
+
+    btnForm.addEventListener('click', function () {
+        formContato.classList.toggle('show-form');
+        overlayMenu.classList.toggle('show-overlay');
+    });
+
+    overlayMenu.addEventListener('click', function () {
+        formContato.classList.remove('show-form');
+        overlayMenu.classList.remove('show-overlay');
+    });
+});
